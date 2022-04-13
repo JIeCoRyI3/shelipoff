3ee957b95b22b46c3ccff276b7acff5084c6496b
Kirill <jiecoryi3@gmail.com>
2022-04-13 19:00:09 +0300

    [initial] Init develop branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+
 import * as Styled from './styles';
 
 const App: FC = () => {
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root'),
